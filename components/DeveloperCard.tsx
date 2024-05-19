2f5d194c0fe11d0ee7426b34dd61922dbe54b0d6
chuhemiao <535101602@qq.com>
2024-05-19 20:21:02 +0800

    add: week1

--- components/DeveloperCard.tsx
+++ components/DeveloperCard.tsx
@@ -1,43 +1,37 @@
-"use client";
+'use client';
 
-import FollowButton from "@/components/FollowButton";
-import { Avatar, Card, CardBody, CardHeader, Chip } from "@nextui-org/react";
+import FollowButton from '@/components/FollowButton';
+import { Avatar, Card, CardBody, CardHeader, Chip } from '@nextui-org/react';
 
 export default function DeveloperCard() {
   return (
-    <div className="flex w-full items-start justify-center mt-12">
-      <Card className="mt-10 w-[400px]">
-        <CardHeader className="relative flex h-[100px] flex-col justify-end overflow-visible bg-gradient-to-br from-indigo-300 via-Cyan-300 to-blue-400">
-          <Avatar className="h-20 w-20 translate-y-12" src="/avatar.jpg" />
+    <div className='flex w-full items-start justify-center mt-12'>
+      <Card className='mt-10 w-[400px]'>
+        <CardHeader className='relative flex h-[100px] flex-col justify-end overflow-visible bg-gradient-to-br from-indigo-300 via-Cyan-300 to-blue-400'>
+          <Avatar className='h-20 w-20 translate-y-12' src='/avatar.jpg' />
         </CardHeader>
         <CardBody>
-          <div className="pb-4 pt-6">
-            <p className="text-large font-medium">weijunext/程普</p>
-            <p className="max-w-[90%] text-small text-default-400">
-              @weijunext
+          <div className='pb-4 pt-6'>
+            <p className='text-large font-medium'>kkdemian/白总</p>
+            <p className='max-w-[90%] text-small text-default-400'>
+              @0xkkdemian
             </p>
-            <div className="flex gap-2 pb-1 pt-2">
-              <Chip variant="flat">👨‍💻前端</Chip>
-              <Chip variant="flat">🛠️全栈</Chip>
-              <Chip variant="flat">✨AI</Chip>
-              <Chip variant="flat">⛵️出海</Chip>
+            <div className='flex gap-2 pb-1 pt-2'>
+              <Chip variant='flat'>👨‍💻前端</Chip>
+              <Chip variant='flat'>🛠️全栈</Chip>
+              <Chip variant='flat'>✨公链</Chip>
+              <Chip variant='flat'>⛵️DAO</Chip>
             </div>
-            <p className="py-2 text-small text-foreground">
-              Next.js开源手艺人｜掘金签约作者｜出海鼓励师
+            <p className='py-2 text-small text-foreground'>
+              ICP生态建设者｜DAO爱好者｜没有群了｜努力挣MBA学费
             </p>
-            <div className="w-full text-center mt-4 flex justify-evenly">
+            <div className='w-full text-center mt-4 flex justify-evenly'>
               <FollowButton
-                name="Twitter/X"
-                href="https://twitter.com/weijunext/"
-              ></FollowButton>
+                name='Twitter/X'
+                href='https://twitter.com/0xkkdemian/'></FollowButton>
               <FollowButton
-                name="Github"
-                href="https://github.com/weijunext/"
-              ></FollowButton>
-              <FollowButton
-                name="掘金"
-                href="https://juejin.cn/user/26044008768029"
-              ></FollowButton>
+                name='Youtube'
+                href='https://www.youtube.com/@kkdemian'></FollowButton>
             </div>
           </div>
         </CardBody>
--- config/site.ts
+++ config/site.ts
@@ -1,70 +1,85 @@
-import { SiteConfig } from "@/types/siteConfig";
-import { BsGithub, BsTwitterX, BsWechat } from "react-icons/bs";
-import { MdEmail } from "react-icons/md";
-import { SiBuymeacoffee, SiJuejin } from "react-icons/si";
+import { SiteConfig } from '@/types/siteConfig';
+import { BsTools, BsTwitterX, BsYoutube } from 'react-icons/bs';
+import { SiBinance } from 'react-icons/si';
 
 const baseSiteConfig = {
-  name: "Weekly Boilerplate",
-  description:
-    "Weekly Boilerplate is a carefully crafted weekly newsletter website template, implemented using Next.js + MDX.",
-  url: "https://weekly.weijunext.com",
+  name: 'Weekly Kkdemian',
+  description: '一个关注加密货币信息和分享自己观点的地方.',
+  url: 'https://weekly.kkdemian.com',
   metadataBase: '/',
-  keywords: ["open-source weekly"],
+  keywords: ['kkdemian 加密货币周刊 加密货币交易 比特币交易'],
   authors: [
     {
-      name: "weijunext",
-      url: "https://weijunext.com",
-      twitter: 'https://twitter.com/weijunext',
+      name: 'kkdemian',
+      url: 'https://weekly.kkdemian.com',
+      twitter: 'https://twitter.com/0xkkdemian'
     }
   ],
-  creator: '@weijunext',
+  creator: '@kkdemian',
   defaultNextTheme: 'dark', // next-theme option: system | dark | light
   icons: {
-    icon: "/favicon.ico",
-    shortcut: "/logo.png",
-    apple: "/logo.png",
+    icon: '/favicon.ico',
+    shortcut: '/logo.jpg',
+    apple: '/logo.jpg'
   },
   headerLinks: [
-    { name: 'repo', href: "https://github.com/weijunext/weekly-boilerplate", icon: BsGithub },
-    { name: 'twitter', href: "https://twitter.com/weijunext", icon: BsTwitterX },
-    { name: 'buyMeCoffee', href: "https://www.buymeacoffee.com/weijunext", icon: SiBuymeacoffee }
+    {
+      name: 'repo',
+      href: 'https://www.youtube.com/@kkdemian',
+      icon: BsYoutube
+    },
+    {
+      name: 'twitter',
+      href: 'https://twitter.com/0xkkdemian',
+      icon: BsTwitterX
+    },
+    {
+      name: 'Tool',
+      href: 'https://tool.kkdemian.com/',
+      icon: BsTools
+    }
   ],
   footerLinks: [
-    { name: 'email', href: "mailto:weijunext@gmail.com", icon: MdEmail },
-    { name: 'twitter', href: "https://twitter.com/weijunext", icon: BsTwitterX },
-    { name: 'github', href: "https://github.com/weijunext/", icon: BsGithub },
-    { name: 'buyMeCoffee', href: "https://www.buymeacoffee.com/weijunext", icon: SiBuymeacoffee },
-    { name: 'juejin', href: "https://juejin.cn/user/26044008768029", icon: SiJuejin },
-    { name: 'weChat', href: "https://weijunext.com/make-a-friend", icon: BsWechat }
+    {
+      name: 'twitter',
+      href: 'https://twitter.com/0xkkdemian',
+      icon: BsTwitterX
+    },
+    // { name: 'github', href: "https://github.com/chuhemiao/", icon: BsGithub },
+    {
+      name: 'binance',
+      href: 'https://accounts.binance.com/en/register?ref=MKOOSEO4',
+      icon: SiBinance
+    }
   ],
   footerProducts: [
-    { url: 'https://weijunext.com/', name: 'J实验室' },
-    { url: 'https://smartexcel.cc/', name: 'Smart Excel' },
-    { url: 'https://weekly.weijunext.com/', name: 'Weekly Boilerplate' },
-    { url: 'https://landingpage.weijunext.com/', name: 'Landing Page Boilerplate' },
-    { url: 'https://nextjs.weijunext.com/', name: 'Next.js Practice' },
-    { url: 'https://starter.weijunext.com/', name: 'Next.js Starter' },
-    { url: 'https://githubbio.com', name: 'Github Bio Generator' },
-    { url: 'https://github.com/weijunext/indie-hacker-tools', name: 'Indie Hacker Tools' },
+    { url: 'https://www.youtube.com/@kkdemian', name: 'Youtube' },
+    { url: 'https://www.internetcomputer.wiki/', name: 'ICP开发指南' },
+    { url: 'https://www.okx.com/join/1871789', name: '注册OKX交易所' },
+    { url: 'https://tool.kkdemian.com/', name: '币圈工具大全' },
+    {
+      url: 'https://accounts.binance.com/en/register?ref=MKOOSEO4',
+      name: '注册Binance'
+    }
   ]
-}
+};
 
 export const siteConfig: SiteConfig = {
   ...baseSiteConfig,
   openGraph: {
-    type: "website",
-    locale: "en_US",
+    type: 'website',
+    locale: 'en_US',
     url: baseSiteConfig.url,
     title: baseSiteConfig.name,
     description: baseSiteConfig.description,
     siteName: baseSiteConfig.name,
-    images: [`${baseSiteConfig.url}/og.jpg`],
+    images: [`${baseSiteConfig.url}/og.jpg`]
   },
   twitter: {
-    card: "summary_large_image",
+    card: 'summary_large_image',
     title: baseSiteConfig.name,
     description: baseSiteConfig.description,
     images: [`${baseSiteConfig.url}/og.jpg`],
-    creator: baseSiteConfig.creator,
-  },
-}
+    creator: baseSiteConfig.creator
+  }
+};
