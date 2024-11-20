2f3b9db5b1816727c8b38483b8f87f1171037fcf
chuhemiao <535101602@qq.com>
2024-11-20 10:47:41 +0800

    ibit

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,24 +1,26 @@
-import FollowButton from "@/components/FollowButton";
-import Image from "next/image";
+import FollowButton from '@/components/FollowButton';
+import Image from 'next/image';
 
 export default function NotFound() {
   return (
-    <div className="mx-automax-h-screen flex flex-col items-center">
-      <Image src="/404.svg" alt="404" width={128} height={128} />
-      <div className="w-full text-center mt-8 flex flex-col sm:flex-row gap-2">
-        <FollowButton name="回到首页" href="/" target="_self"></FollowButton>
+    <div className='mx-automax-h-screen flex flex-col items-center'>
+      <Image src='/404.svg' alt='404' width={128} height={128} />
+      <div className='w-full text-center mt-8 flex flex-col sm:flex-row gap-2'>
+        <FollowButton name='回到首页' href='/' target='_self'></FollowButton>
         <FollowButton
-          name="Twitter/X"
-          href="https://twitter.com/weijunext/"
-        ></FollowButton>
+          name='X'
+          href='https://twitter.com/0xkkdemian'></FollowButton>
+
         <FollowButton
-          name="Github"
-          href="https://github.com/weijunext/"
-        ></FollowButton>
+          name='注册币安'
+          href='https://accounts.binance.com/en/register?ref=MKOOSEO4'></FollowButton>
+
         <FollowButton
-          name="掘金"
-          href="https://juejin.cn/user/26044008768029"
-        ></FollowButton>
+          name='kkdemian'
+          href='https://kkdemian.com/'></FollowButton>
+        <FollowButton
+          name='币圈工具'
+          href='https://tool.kkdemian.com/'></FollowButton>
       </div>
     </div>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,23 @@
-import createMDX from "@next/mdx";
-import remarkGfm from "remark-gfm";
+import createMDX from '@next/mdx';
+import remarkGfm from 'remark-gfm';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configure `pageExtensions`` to include MDX files
-  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true
+  }
 };
 
 const withMDX = createMDX({
   // Add markdown plugins here, as desired
   options: {
     remarkPlugins: [remarkGfm],
-    rehypePlugins: [],
-  },
+    rehypePlugins: []
+  }
 });
 
 // Wrap MDX and Next.js config with each other
